fe906282f2b64f97ed90bc88a224cc218d177900
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-04 09:52:15 -0500

    updates to try and fix multi sensor interference

--- sensor.py
+++ sensor.py
@@ -31,12 +31,13 @@ class sensors():
 
     while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
       pulse_start = time.time()              #Saves the last known time of LOW pulse
-    print 'pulse start time =%s'%(pulse_start)
     while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
       pulse_end = time.time()                #Saves the last known time of HIGH pulse
-    print 'pulse end time =%s'%(pulse_end)
+
 
     pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
+    print 'pulse start time =%s' % (pulse_start)
+    print 'pulse end time =%s' % (pulse_end)
     print 'pulse duration = %s'%(pulse_duration)
     distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
     distance = round(distance, 2)            #Round to two decimal points
