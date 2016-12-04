0253d62c5bfefce4ea6f3c9deac80f0156015879
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-04 15:17:50 -0500

    updates to try and fix multi sensor interference

--- autonomous.py
+++ autonomous.py
@@ -7,11 +7,14 @@ left_sensor = snsr.forward_distance()[0]
 right_sensor = snsr.forward_distance()[1]
 
 while True:
-    arbot.forward(115)
-    if snsr.forward_distance()[0] <= 10 or snsr.forward_distance()[0] == None:
-        arbot.right_turn(155,1)
-        pass
-    if snsr.forward_distance()[1] <= 10 or snsr.forward_distance()[1] == None:
-        arbot.left_turn(155,1)
-        pass
+    try:
+        arbot.forward(115)
+        if snsr.forward_distance()[0] <= 10 or snsr.forward_distance()[0] == None:
+            arbot.right_turn(155,1)
+            pass
+        if snsr.forward_distance()[1] <= 10 or snsr.forward_distance()[1] == None:
+            arbot.left_turn(155,1)
+            pass
+    except KeyboardInterrupt:
+        arbot.turnOffMotors()
 
