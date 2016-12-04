22fc5b61de65ac29aa5affbe55b08866392075c9
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-04 15:35:09 -0500

    updates to try and fix multi sensor interference

--- autonomous.py
+++ autonomous.py
@@ -6,18 +6,20 @@ snsr = sensor.sensors()
 left_sensor = snsr.forward_distance()[0]
 right_sensor = snsr.forward_distance()[1]
 crusing_speed = 115
+stopping_distance = 15
+
 while True:
     try:
         print 'Forward'
         arbot.forward(155)
         arbot.forward(crusing_speed)
 
-        if snsr.forward_distance()[0] <= 10 or snsr.forward_distance()[0] == None:
+        if snsr.forward_distance()[0] <= stopping_distance or snsr.forward_distance()[0] == None:
             print 'left sensor proximity warning: making right turn'
             arbot.stop(crusing_speed)
             arbot.right_turn(255,1)
             pass
-        if snsr.forward_distance()[1] <= 10 or snsr.forward_distance()[1] == None:
+        if snsr.forward_distance()[1] <= stopping_distance or snsr.forward_distance()[1] == None:
             print 'right sensor proximity warning: making left turn'
             arbot.stop(crusing_speed)
             arbot.left_turn(255,1)
