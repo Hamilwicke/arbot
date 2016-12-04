f2e637070ae52605fdc6f532b19899634550ff00
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-04 14:09:21 -0500

    updates to try and fix multi sensor interference

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import time
 sn = sensor.sensors()
 
 while True:
-    # print '============='
-    print sn.forward_distance()
-    #print sn.get_multi_sensor_distance()
-    # print '============='
+    print '============='
+    #print sn.forward_distance()
+    print sn.get_multi_sensor_distance()
+    print '============='
     time.sleep(.1)
 
