dd0ff2b67ae15548d40717565f94d2c04191c339
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-03 21:05:02 -0500

    Pushing minor updates

--- sensor.py
+++ sensor.py
@@ -1,20 +1,31 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO                    #Import GPIO library
+import time                                #Import time library
+GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering
+
+LEFT_TRIG = 20                                  #Associate pin 23 to TRIG
+LEFT_ECHO = 21                                  #Associate pin 24 to ECHO
+RIGHT_TRIG = 16
+RIGHT_ECHO = 19
+
+print "Distance measurement in progress"
+
+GPIO.setup(LEFT_TRIG, GPIO.OUT)                  #Set pin as GPIO out
+GPIO.setup(LEFT_ECHO, GPIO.IN)                   #Set pin as GPIO in
 
 while True:
 
-  GPIO.output(TRIG, False)                 #Set TRIG as LOW
+  GPIO.output(LEFT_TRIG, False)                 #Set TRIG as LOW
   print "Waitng For Sensor To Settle"
   time.sleep(2)                            #Delay of 2 seconds
 
-  GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+  GPIO.output(LEFT_TRIG, True)                  #Set TRIG as HIGH
   time.sleep(0.00001)                      #Delay of 0.00001 seconds
-  GPIO.output(TRIG, False)                 #Set TRIG as LOW
+  GPIO.output(LEFT_TRIG, False)                 #Set TRIG as LOW
 
-  while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
+  while GPIO.input(LEFT_ECHO)==0:               #Check whether the ECHO is LOW
     pulse_start = time.time()              #Saves the last known time of LOW pulse
 
-  while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
+  while GPIO.input(LEFT_ECHO)==1:               #Check whether the ECHO is HIGH
     pulse_end = time.time()                #Saves the last known time of HIGH pulse
 
   pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
