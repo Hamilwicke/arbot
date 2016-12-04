698dc317ca86c8027f0d2045f1c2b5993024e725
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-04 09:58:39 -0500

    updates to try and fix multi sensor interference

--- sensor.py
+++ sensor.py
@@ -20,9 +20,9 @@ class sensors():
 
 
   def get_distance(self, (TRIG, ECHO)):
-
+    time.sleep(1)
     GPIO.output(TRIG, False)                 #Set TRIG as LOW
-
+    time.sleep(1)
     print 'triggering pulse'
     GPIO.output(TRIG, True)                  #Set TRIG as HIGH
     time.sleep(0.00001)                      #Delay of 0.00001 seconds
