64cbebd7b17f5d07d01f00ac209e3fe7a94b60cb
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-04 14:47:42 -0500

    updates to try and fix multi sensor interference

--- autonomous.py
+++ autonomous.py
@@ -7,7 +7,7 @@ left_sensor = snsr.forward_distance()[0]
 right_sensor = snsr.forward_distance()[1]
 
 while True:
-    if snsr.forward_distance() >= 10:
+    if snsr.forward_distance()[0] >= 10 and snsr.forward_distance()[1] >= 10:
         arbot.forward(125)
 
     else:
