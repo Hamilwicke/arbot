61f5f482d7564d0ae60588efba6084fdf46ac606
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-06 06:12:54 -0500

    Adding servo control

--- servo.py
+++ servo.py
@@ -7,13 +7,13 @@ GPIO.setup(servoPin, GPIO.OUT)
 pwm = GPIO.PWM(servoPin, 50)
 pwm.start(7)
 
-'''
-    for i in range(0,20):
-    dc = input('pick number 1-20')
-    pwm.ChangeDutyCycle(dc)
-'''
+
+for i in range(0,20):
+dc = input('pick number 1-20')
+pwm.ChangeDutyCycle(dc)
 
 
+'''
 for i in range(0,180):
 
     DC = 1./20.*(i)+3.1
@@ -23,6 +23,6 @@ for i in range(0,180):
     else:
         time.sleep(.1)
     print i
-
+'''
 pwm.stop()
 GPIO.cleanup()
