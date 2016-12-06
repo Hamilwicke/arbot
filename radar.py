aa7fefddf0f4af973b743db2f5441d68f4c229e8
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-06 07:15:13 -0500

    Adding servo control

--- radar.py
+++ radar.py
@@ -14,14 +14,14 @@ pwm.start(7)
 
 while True:
 
-    for i in range(10,170):
+    for i in range(10,160):
         DC = 1. / 20. * (i) + 3
         pwm.ChangeDutyCycle(DC)
         dist = snsr.get_distance((snsr.LEFT_TRIG,snsr.LEFT_ECHO))
         print 'angle: %s, distance: %s' % (i, dist)
     time.sleep(.05)
 
-    for i in reversed(range(10,170)):
+    for i in reversed(range(10,160)):
         DC = 1./20.*(i)+3
         pwm.ChangeDutyCycle(DC)
         dist = snsr.get_distance((snsr.LEFT_TRIG, snsr.LEFT_ECHO))
