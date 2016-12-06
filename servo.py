3607f6423ab16ea899e3683efcc63a733a4c88d0
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-05 20:30:10 -0500

    Adding servo control

--- servo.py
+++ servo.py
@@ -1,13 +1,15 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 servoPin = 4
 GPIO.setup(servoPin, GPIO.OUT)
 pwm = GPIO.PWM(servoPin, 50)
 pwm.start(7)
-for i in range(0,20):
-    desiredPosition = input("servo position? 0-180: ")
-    DC = 1/18.*(desiredPosition)+2
+for i in range(0,180):
+    DC = 1/18.*(i)+2
     pwm.ChangeDutyCycle(DC)
+    time.sleep(.1)
+    print i
 pwm.stop()
 GPIO.cleanup()
