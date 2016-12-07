13547ac1c7f629c4b43cb85d7d9642457b9abd93
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-07 18:58:38 -0500

    Adding servo control

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import sensor
 import time
-import us2
+
 
 sn = sensor.sensors()
 
 while True:
-    for
     #print '============='
     print 'left = %s, right = %s' % (sn.forward_left(),sn.forward_right())
     #print sn.get_multi_sensor_distance()
