44a9e593508b208952a9c81f94e947ea2faa7721
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-07 19:43:48 -0500

    Adding servo control

--- test.py
+++ test.py
@@ -6,7 +6,9 @@ sn = sensor.sensors()
 
 while True:
     #print '============='
-    print 'left = %s, right = %s' % (sn.forward_left(),sn.forward_right())
+    print 'left = %s, right = %s' % (sn.forward_left()
+                                     #,sn.forward_right()
+                                     )
     #print sn.get_multi_sensor_distance()
     #print sn.get_multi_sensor_distance()
     #print '============='
