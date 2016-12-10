da1609db2aa9a644fa4375a728a6bc64a934eeca
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-09 21:39:51 -0500

    Final touches on sensor, adding functionality for all three.

--- stepper.py
+++ stepper.py
@@ -9,7 +9,7 @@ GPIO.setmode(GPIO.BCM)
 # Define GPIO signals to use
 # Physical pins 11,15,16,18
 # GPIO17,GPIO22,GPIO23,GPIO24
-StepPins = [17, 18, 27, 24]
+StepPins = [17, 18, 27, 22]
 
 # Set all pins as output
 for pin in StepPins:
