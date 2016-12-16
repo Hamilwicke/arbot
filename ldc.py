4d771c1e9525dee688d7dccb9096821ada5672a3
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-15 21:57:26 -0500

    Final touches on sensor, adding functionality for all three.

--- ldc.py
+++ ldc.py
@@ -20,13 +20,15 @@
 import RPi.GPIO as GPIO
 import time
 
+# 4grey,17purp,18blue,27green,22yell,23oran,24red,25brown,5black,
+
 # Define GPIO to LCD mapping
-LCD_RS = 27
+LCD_RS = 23
 LCD_E = 22
-LCD_D4 = 25
-LCD_D5 = 24
-LCD_D6 = 23
-LCD_D7 = 18
+LCD_D4 = 27
+LCD_D5 = 18
+LCD_D6 = 17
+LCD_D7 = 4
 
 # Define some device constants
 LCD_WIDTH = 16  # Maximum characters per line
