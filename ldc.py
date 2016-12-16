8411391ca29cf4f95652b444360ba77a6c908fb8
MikeHardwickeBrown <michaelhardwickebrown@gmail.com>
2016-12-16 07:37:48 -0500

    Final touches on sensor, adding functionality for all three.

--- ldc.py
+++ ldc.py
@@ -61,14 +61,14 @@ def main():
     while True:
         print 'loop'
         # Send some test
-        #lcd_string("    OH SHIT!    ", LCD_LINE_1)
-        #lcd_string("I'm Up Side Down", LCD_LINE_2)
+        lcd_string("    OH SHIT!    ", LCD_LINE_1)
+        lcd_string("I'm Up Side Down", LCD_LINE_2)
 
         #time.sleep(3)  # 3 second delay
 
         # Send some text
-        lcd_string("Daddy loves Mommy", LCD_LINE_1)
-        lcd_string("", LCD_LINE_2)
+        #lcd_string("Daddy loves Mommy", LCD_LINE_1)
+        #lcd_string("", LCD_LINE_2)
 
         #time.sleep(3)  # 3 second delay
 
